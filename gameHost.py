8c1207dd803f5a76347aa0285813f5a4ca6b64ef
FKH886 <foukaihe@gmail.com>
2021-05-15 01:22:39 -0400

    Host method: get pool added

--- gameHost.py
+++ gameHost.py
@@ -5,22 +5,28 @@ import string
 class Host:
     def __init__(self, duplicate=False, column=4, pool=6):
         print('duplicate: ', duplicate, '  column: ', column, '  pool: ', pool)
-        balls = []
+        self.__balls = []
         if pool == 6:
-            balls = ['B', 'R', 'Y', 'G', 'P', 'W']
+            __balls = ['B', 'R', 'Y', 'G', 'P', 'W']
         else:
-            for i in range(pool): balls.append(string.ascii_uppercase[i])
-        print(balls)
+            for i in range(pool): self.__balls.append(string.ascii_uppercase[i])
+        print(self.__balls)
         if duplicate:
             selected_balls = []
             for i in range(column):
-                selected_balls += random.sample(balls, 1)
+                selected_balls += random.sample(self.__balls, 1)
         else:
-            selected_balls = random.sample(balls, column)
-        self._hidden_box = selected_balls
+            selected_balls = random.sample(self.__balls, column)
+        self.__hidden_box = selected_balls
 
     def show_hidden_box(self):
-        print(self._hidden_box)
+        print(self.__hidden_box)
+
+    def get_pool(self):
+        return self.__balls
+
+    def get_hidden_box(self):
+        return self.__hidden_box
 
     def guess_input(self):
         guess_balls = input("enter your guess: ")
@@ -31,12 +37,12 @@ class Host:
         blow = 0
         temp_guess = list(guess_balls)
         for i in range(4):
-            if self._hidden_box[i] == guess_balls[i]:
+            if self.__hidden_box[i] == guess_balls[i]:
                 hit += 1
-                temp_guess.remove(self._hidden_box[i])
-            elif self._hidden_box[i] in temp_guess:
+                temp_guess.remove(self.__hidden_box[i])
+            elif self.__hidden_box[i] in temp_guess:
                 blow += 1
-                temp_guess.remove(self._hidden_box[i])
+                temp_guess.remove(self.__hidden_box[i])
 
         if hit > 1:
             print(hit, "Hits, ", end='')
@@ -55,10 +61,10 @@ class Host:
         hit = 0
         blow = 0
         for i in range(4):
-            if self._hidden_box[i] == guess_balls[i]:
+            if self.__hidden_box[i] == guess_balls[i]:
                 hit += 1
                 blow += 1
-            elif self._hidden_box[i] in guess_balls:
+            elif self.__hidden_box[i] in guess_balls:
                 blow += 1
 
-        return (hit, blow)
+        return hit, blow
